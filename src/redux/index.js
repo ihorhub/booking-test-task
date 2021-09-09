911dc5e3d4e2fca769fdcc6df03f2b9e4df3e64d
ihorhub <ihor_babiy@ukr.net>
2021-09-10 01:21:39 +0300

    renemae fold

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,3 +3,5 @@ import reducer from './reducers/counter-reducer'
 import thunk from 'redux-thunk'
 
 export const store = createStore(reducer, applyMiddleware(thunk))
+export * from './action-types'
+export * from './action-creators'
--- src/redux/reducers/counter-reducer.js
+++ src/redux/reducers/counter-reducer.js
@@ -3,6 +3,7 @@ import { DEC_COUNTER, INC_COUNTER } from '../action-types'
 const initialState = { counter: 0 }
 
 const reducer = (state = initialState, action) => {
+
   switch (action.type) {
     case INC_COUNTER: {
       return { ...state, counter: state.counter + 2 }
